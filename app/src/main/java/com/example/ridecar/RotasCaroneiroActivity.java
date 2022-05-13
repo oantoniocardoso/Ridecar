8bb87af255b8714e455caa6f4cfd967b808ffee9
Adriano-Belle <Sepherot1234>
2022-05-12 21:44:29 -0300

    adiciona rotas caroneiros

---
+++ app/src/main/java/com/example/ridecar/RotasCaroneiroActivity.java
@@ -0,0 +1,14 @@
+package com.example.ridecar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RotasCaroneiroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rotas_caroneiro);
+    }
+}
