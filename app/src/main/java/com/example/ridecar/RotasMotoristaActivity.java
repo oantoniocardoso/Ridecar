884ed1e6692d6a41df99ae982e8cd0999ba6656d
Antonio Cardoso <antonioc_contas@hotmail.com>
2022-05-19 01:38:46 -0300

    14
    Ajuste mapa.

--- app/src/main/java/com/example/ridecar/RotasMotoristaActivity.java
+++ app/src/main/java/com/example/ridecar/RotasMotoristaActivity.java
@@ -1,20 +1,37 @@
 package com.example.ridecar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class RotasMotoristaActivity extends AppCompatActivity {
+public class RotasMotoristaActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_rotas_motorista);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng Uniftec = new LatLng(-29.173624386319837, -51.218501087199);
+        googleMap.addMarker(new MarkerOptions().position(Uniftec).title("Uniftec"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(Uniftec));
     }
 
     public void botaoVoltarOnClick(View v) { startActivity(new Intent(this, CaroneiroActivity.class));}
